6240f6b0971e32b08f74ef02834917b98ded906d
Diego Siqueira <mail@diegosiqueira.xyz>
2023-10-22 18:55:45 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
 
+import {
+  ExportAsService,
+  ExportAsConfig,
+  SupportedExtensions,
+} from 'ngx-export-as';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-export-app';
+  downloadAs: SupportedExtensions = 'png';
+
+  exportAsConfig: ExportAsConfig = {
+    type: 'xlsx', // the type you want to download
+    elementIdOrContent: 'sampleTable', // the id of html/table element
+  };
+
+  constructor(private exportAsService: ExportAsService) {}
+
+  export() {
+    this.exportAsConfig.type = this.downloadAs;
+    // download the file using old school javascript method
+    this.exportAsService
+      .save(this.exportAsConfig, 'Exported_File_Name')
+      .subscribe(() => {
+        // save started
+      });
+    // get the data as base64 or json object for json type - this will be helpful in ionic or SSR
+    this.exportAsService.get(this.exportAsConfig).subscribe((content) => {
+      console.log(content);
+    });
+  }
+
+  exportCSV() {
+    this.exportType('csv')
+  }
+
+  exportPDF() {
+    this.exportType('pdf')
+  }
+
+  exportXLSX() {
+    this.exportType('xlsx')
+  }
+
+  exportType(format : SupportedExtensions) {
+    this.exportAsConfig.type = format;
+    this.exportAsService.save(this.exportAsConfig, 'Exported_File_Name').subscribe(() => {
+      // save started
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { ExportAsModule } from 'ngx-export-as';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    ExportAsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
